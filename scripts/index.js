e2af7ca06360318069d02f0261b55b6bfbd92e98
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2022-07-03 14:26:27 +0400

    The second correction after review

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,7 @@ const aboutUserInput = formProfile.querySelector('.form__item_input_about');
 const urlInput = formCards.querySelector('.form__item_input_url');
 const namePlaceInput = formCards.querySelector('.form__item_input_place');
 
-//***Функции открывания и закрывания форм по кнопкам ***
+//***Функции открывания и закрывания форм по кнопкам ***//
 
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened');
@@ -30,29 +30,38 @@ function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
 }
 
-//***Функция создания карточки из шаблона***
+//***Функция переключения кнопки лайка***//
+
+function toggleLikeButton(button) {
+  button.addEventListener('click', evt => {
+    evt.target.classList.toggle('card__button_active');
+  });
+}
+
+//***Функция удаления карточки по кнопке***//
+
+function deleteCardButton(button) {
+  button.addEventListener('click', evt => {
+    evt.target.closest('.card').remove();
+  });
+}
+
+//***Функция создания карточки из шаблона***//
 
 function createCard(link, name) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
 
-  cardElement.querySelector('.card__image').src = link;
+  cardImage.src = link;
+  cardImage.alt = name;
   cardElement.querySelector('.card__place').textContent = name;
-  cardElement.querySelector('.card__image').alt = name;
 
-  //Переключение кнопки лайка
-  cardElement.querySelector('.card__button').addEventListener('click', evt => {
-    evt.target.classList.toggle('card__button_active')
-  });
+  toggleLikeButton(cardElement.querySelector('.card__button'));
 
-  //Удаление карточки по кнопке
-  cardElement.querySelectorAll('.card__delete-button').forEach(item => {
-    item.addEventListener('click', evt => {
-      evt.target.closest('.card').remove();
-    })
-  });
+  deleteCardButton(cardElement.querySelector('.card__delete-button'));
 
   //Открывание попапа с картинкой
-  cardElement.querySelector('.card__image').addEventListener('click', () => {
+  cardImage.addEventListener('click', () => {
     popupImage.src = link;
     popupImageName.textContent = name;
     popupImage.alt = name;
@@ -63,13 +72,13 @@ function createCard(link, name) {
   return cardElement;
 }
 
-//***Функция добавления карточки в DOM***
+//***Функция добавления карточки в DOM***//
 
 function addCard(container, element) {
   container.prepend(element);
 }
 
-//***Функция сохранения информации о пользователе из формы***
+//***Функция сохранения информации о пользователе из формы***//
 
 function handleProfileFormSubmit (evt) {
   evt.preventDefault();
@@ -80,27 +89,29 @@ function handleProfileFormSubmit (evt) {
   closePopup(popupProfile);
 }
 
-//***Функция добавления новой карточки из формы***
+//***Функция добавления новой карточки из формы***//
 
 function handleCardFormSubmit (evt) {
   evt.preventDefault();
 
   addCard(cardsGallery, createCard(urlInput.value, namePlaceInput.value));
 
-  document.querySelector('.form_type_cards').reset();
+  formCards.reset();
 
   closePopup(popupGallery);
 }
 
-//***Обработка отправки форм***
+//***Обработка отправки форм***//
 
 formProfile.addEventListener('submit', handleProfileFormSubmit);
 formCards.addEventListener('submit', handleCardFormSubmit);
 
-//***Открывание/закрывание попапов***
+//***Открывание/закрывание попапов***//
 
 openProfileButton.addEventListener('click', () => {
   openPopup(popupProfile);
+  userNameInput.value = profileName.textContent;
+  aboutUserInput.value = profileDescription.textContent;
 });
 
 openAddCardButton.addEventListener('click', () => {
@@ -119,7 +130,7 @@ closePhotoButton.addEventListener('click', () => {
   closePopup(popupPhoto);
 });
 
-//***Создание карточек из массива данных***
+//***Создание карточек из массива данных***//
 
 initialCards.forEach(function(item){
   addCard(cardsGallery, createCard(item.link, item.name));
