9af46b57d4f67ee427659f2d7599cc11a8acf120
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2022-09-08 22:28:13 +0400

    Complete GET_PATCH_profile and GET_cards

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,62 @@
-import {initialCards,addCard, createCard} from './card.js';
-import {formProfile, formCards, cardsGallery, urlInput, namePlaceInput, profileName, profileDescription, userNameInput, aboutUserInput, popupProfile, popupGallery, closePopup} from './modal.js';
+import {/*initialCards,*/addCard, createCard} from './card.js';
+import {formProfile, formCards, cardsGallery, urlInput, namePlaceInput, profileName, profileDescription, profileAvatar, userNameInput, aboutUserInput, popupProfile, popupGallery, closePopup, disableButton} from './modal.js';
 import {enableValidation, validateSettings} from './validate.js';
 import '../pages/index.css';
+
+import { getInitialCards, getProfile, editProfile } from './api.js'
+
+getProfile()
+  .then((result) => {
+    console.log(result);
+    profileName.textContent = result.name;
+    profileDescription.textContent = result.about;
+    profileAvatar.img = result.avatar;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/*fetch('https://nomoreparties.co/v1/plus-cohort-14/users/me', {
+  headers: {
+    method: 'GET',
+    authorization: '1a1c4ff3-29e4-400a-a3cb-d6bec25bd6e2'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+    profileName.textContent = result.name;
+    profileDescription.textContent = result.about;
+    profileAvatar.img = result.avatar;
+  });*/
 
 //***Функция сохранения информации о пользователе из формы***//
 function handleProfileFormSubmit (evt) {
   evt.preventDefault();
 
-  const buttonSubmit = formProfile.querySelector('.form__button');
+  editProfile(userNameInput, aboutUserInput)
+    //.then(getProfile())
+    .then((result) => {
+      console.log(result);
+      profileName.textContent = result.name;
+      profileDescription.textContent = result.about;
+      profileAvatar.img = result.avatar;
+    })
 
-  profileName.textContent = userNameInput.value;
-  profileDescription.textContent = aboutUserInput.value;
+    /*.then((result) => {
+      console.log(result);
+      // обрабатываем результат
+      result.name = userNameInput.value;
+      result.about = aboutUserInput.value;
+    })*/
+    .catch((err) => {
+      console.log(err);
+    });
 
-  if (!buttonSubmit.hasAttribute('disabled')) {
-    buttonSubmit.setAttribute('disabled', true)
-  } else {
-    buttonSubmit.removeAttribute('disabled')
-  }
+  /*profileName.textContent = userNameInput.value;
+  profileDescription.textContent = aboutUserInput.value;*/
+
+  disableButton();
 
   closePopup(popupProfile);
 }
@@ -24,17 +64,12 @@ function handleProfileFormSubmit (evt) {
 //***Функция добавления новой карточки из формы***//
 function handleCardFormSubmit (evt) {
   evt.preventDefault();
-  const buttonSubmit = formCards.querySelector('.form__button');
 
   addCard(cardsGallery, createCard(urlInput.value, namePlaceInput.value));
 
   formCards.reset();
 
-  if (!buttonSubmit.hasAttribute('disabled')) {
-    buttonSubmit.setAttribute('disabled', true)
-  } else {
-    buttonSubmit.removeAttribute('disabled')
-  }
+  disableButton();
 
   closePopup(popupGallery);
 }
@@ -44,9 +79,34 @@ formProfile.addEventListener('submit', handleProfileFormSubmit);
 formCards.addEventListener('submit', handleCardFormSubmit);
 
 //***Создание карточек из массива данных***//
-initialCards.forEach(function(item){
-  addCard(cardsGallery, createCard(item.link, item.name));
-});
+getInitialCards()
+  .then((result) => {
+    console.log(result);
+    // обрабатываем результат
+    result.forEach((item) => {
+      addCard(cardsGallery, createCard(item.link, item.name));
+    })
+  })
+  .catch((err) => {
+    console.log(err); // выводим ошибку в консоль
+  });
 
 //***Валидация форм***//
 enableValidation(validateSettings);
+
+/*initialCards.forEach(function(item){
+  /*fetch('https://nomoreparties.co/v1/plus-cohort-14/cards', {
+    headers: {
+      method: 'GET',
+      authorization: '1a1c4ff3-29e4-400a-a3cb-d6bec25bd6e2'
+    }
+  })
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+    item.link = result.link;
+    item.name = result.name;
+
+  });*/
+  /*addCard(cardsGallery, createCard(item.link, item.name));
+});*/
